4a76fe98c19d6f0c34d67fe524930dd1b851fe72
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-07-18 00:35:19 -0400

    Find The Median Answered

--- unit_1/Week2/answers.js/Week2HW.js
+++ unit_1/Week2/answers.js/Week2HW.js
@@ -1,154 +1,157 @@
 // ////////////////////////////////
 // //Easy Going
-// // for (let a = 1; a<=20; a++) {
-// //     console.log(a)
-// // }
-// //////////////////////////////// 
+///////////////////////////////////
+for (let a = 1; a<=20; a++) {
+    console.log(a)
+}
+
 
 // ////////////////////////////////
 // //Get Even
 // ///////////////////////////////
-// // for (let b = 0; b<=200; b+=2){
-// //     console.log(b)
-// // }
+for (let b = 0; b<=200; b+=2){
+    console.log(b)
+}
 
 // ////////////////////////////////
 // // Fizz Buzz
 // ////////////////////////////////
 
-// // for (c = 0; c<=100; c++) {
-// //     if (c % 3 === 0 && c % 5 ===0 ) {
-// //         console.log('FizzBuzz')
-// //     }
-// //     else if (c % 3 ===0) {
-// //         console.log('Fizz');
-// //     }
-// //     else if (c % 5 === 0) {
-// //         console.log('Buzz');
-// //     }
-// //     else {console.log(c)}
-// // }
+for (c = 0; c<=100; c++) {
+    if (c % 3 === 0 && c % 5 ===0 ) {
+        console.log('FizzBuzz')
+    }
+    else if (c % 3 ===0) {
+        console.log('Fizz');
+    }
+    else if (c % 5 === 0) {
+        console.log('Buzz');
+    }
+    else {console.log(c)}
+}
 
 // ///////////////////////////////
 // // Wild Wild Life
 // ///////////////////////////////
 
 // // 1-Plantee just had her birthday; change Plantee's array to reflect her being a year older.
-// const plantee = ["Plantee", "plant",  5000 , "Mordor"];
-// plantee[2] += 1;
+const plantee = ["Plantee", "plant",  5000 , "Mordor"];
+plantee[2] += 1;
 
 // //2-Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
-// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-// wolfy[3] = 'Gotham City'
-// console.log(wolfy)
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+wolfy[3] = 'Gotham City'
+console.log(wolfy)
 
 // //3-Give D'Art a second hometown by adding "Hawkins"
-// // const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
-// // dart.splice (4, 0, 'Hawkins')
-// // console.log(dart)
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+dart.splice (4, 0, 'Hawkins')
+console.log(dart)
 
 // //4-Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
 
-// // wolfy.splice(0, 1, 'Gameboy')
-// // console.log(wolfy)
+wolfy.splice(0, 1, 'Gameboy')
+console.log(wolfy)
 
 // ///////////////////////////////
 // //Yell At The Ninja Turtles
 // ///////////////////////////////
 
 // //NEED HELP******
-// // ninjaTurtle = ["donatello", "leonardo", "raphael" , "michaelangelo"]
-// // for (d = 0; d<ninjaTurtle.length; d++) {
-// //     first_letter=ninjaTurtle[d][0]
-// //     if 
+ninjaTurtle = ["Donatello", "Leonardo", "Raphael" , "Michaelangelo"]
+
+for (let q=0; q<ninjaTurtle.length; q++) {
+
+console.log(ninjaTurtle.UpperCase)
+
+}
+
+
 
 
 // ///////////////////////////////////
 // // METHODS REVISITED
 // ////////////////////////////////// 
 
-// // const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
-// // //console log Titanic
-// // console.log(favMovies[8])
-// // //.sort
-// // favMovies.sort();
-// // console.log(favMovies)
-// // //.pop
-// // favMovies.pop();
-// // console.log(favMovies)
-// // //.push Guardians
-// // favMovies.push('Guardians Of The Galaxy')
-// // console.log(favMovies)
-// // //.reverse
-// // favMovies.reverse()
-// // console.log(favMovies)
-// // //.shift
-// // favMovies.shift()
-// // console.log(favMovies)
-// // //.unshift
-// // favMovies.unshift()
-// // console.log(favMovies)
-// // //.splice
-// // console.log(favMovies.indexOf('Django Unchained'))
-// // favMovies.splice(14,14, 'Avatar')
-// // console.log(favMovies)
-// // //.slice last half of array
-// // console.log(favMovies.length)
-// // favMovies.slice(8,15)
-// // console.log(favMovies)
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+//console log Titanic
+console.log(favMovies[8])
+//.sort
+favMovies.sort();
+console.log(favMovies)
+//.pop
+favMovies.pop();
+console.log(favMovies)
+//.push Guardians
+favMovies.push('Guardians Of The Galaxy')
+console.log(favMovies)
+//.reverse
+favMovies.reverse()
+console.log(favMovies)
+//.shift
+favMovies.shift()
+console.log(favMovies)
+//.unshift
+favMovies.unshift()
+console.log(favMovies)
+//.splice
+console.log(favMovies.indexOf('Django Unchained'))
+favMovies.splice(14,14, 'Avatar')
+console.log(favMovies)
+//.slice last half of array
+console.log(favMovies.length)
+favMovies.slice(8,15)
+console.log(favMovies)
 
 ////////////////////////////////
 // Where is Waldo
 ////////////////////////////////
 
-//Remove Eggbert 
+// //Remove Eggbert 
 
 const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
                     ["Lucinda", "Jacc", "Neff", "Snoop"],
                     ["Petunia", ["Baked Goods", "Waldo"]]];
 
-console.log(whereIsWaldo.indexOf('Eggbert'))
 whereIsWaldo.splice(1, 1)
 console.log(whereIsWaldo)
 
-//Change Neff to No One
+// //Change Neff to No One
 
  whereIsWaldo [1] [2]= "No One"
  console.log(whereIsWaldo)
 
- //access + console log Waldo
+//  //access + console log Waldo
 console.log(whereIsWaldo [2] [1] [1])
+
 
 //////////////////////////////////////
 // Excited Kitten
 /////////////////////////////////////
 
 //Array of Talking Points
-// const catSays= ['...human...why you taking pictures of me?...', '...the catnip made me do it...', '...why does the red dot always get away...']
-
-// for (let m= 0; m < catSays.length; m++); {
-//     console.log(catSays);
-    
-//     }
 
 const catTalk = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..." ];
 
 
-for (let meow = 0; meow < 20; meow++){
+for (let i = 0; i < 20; i++){
   
  console.log("Love me, pet me! HSSSSSSSS");
-if (meow % 2 === 0) {
-    let talkingPoints = Math.round(Math.random() * (catTalk.length));
-    console.log(catTalk[talkingPoints])
+ if ( i % 2 === 0 )
+ {
+   let talkingPoints = Math.floor(Math.random()* (catTalk.length));
+   console.log(catTalk[talkingPoints]);
+ } 
 }
-}
- 
-//  if ( i % 2 === 0 ){
-//    let talkingPoints = Math.floor(Math.random()* (catTalk.length));
-//    console.log(catTalk[talkingPoints]);
-//  }
-// }
 
+/////////////////////////////////
+//Find Median
+/////////////////////////////////
 
+const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+
+nums.sort()
+console.log(nums)
+console.log(nums[Math.floor(nums.length/2)])
 
 
