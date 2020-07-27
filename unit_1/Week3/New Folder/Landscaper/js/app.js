bc09f1ab190e9c94686459ebe0a7922bf51bd652
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-07-26 23:23:24 -0400

    datatype refresher complete

--- unit_1/Week3/New Folder/Landscaper/js/app.js
+++ unit_1/Week3/New Folder/Landscaper/js/app.js
@@ -1,7 +1,120 @@
-let purse = 0
-purse += 1
-console.log(purse)
-purse +=5
-console.log(purse)
-purse +=3
-console.log(purse)
+// // tools = ['teeth', 'rusty scissors']
+// // currentTool = tools[1]
+// // let purse = 0
+// // if (currentTool === 'teeth') {
+// //     purse +=1
+// // }
+// // purse += 1
+// // console.log(purse)
+// // purse +=5
+// // console.log(purse)
+// // purse +=3
+// // console.log(purse)
+
+
+// // const yourAnswer = prompt('which tool you want to use?', 'teeth/scissors/saw');
+
+// // if(yourAnswer == 'teeth'){
+// //   console.log('it is gonna take all day!');
+// // } else if (yourAnswer == 'scissors'){
+// //   console.log('cheapass!!');
+// // }
+// // else if (yourAnswer == 'saw')
+// // {console.log('you are scaring me');
+// // }
+// // else {console.log('does not compute')}
+
+// // const greet = () => {
+// //     alert('hi');
+// // }
+
+// // const byeBye = () => {
+// //     alert('bye')
+// // }
+
+// // const other = () => {
+// //     alert('imshe')
+// // }
+
+// // const answer = prompt ('are you coming or going?', 'coming or going');
+
+// // if (answer === 'coming') {
+// //     greet()
+
+// // } else if (answer ==='going') {
+// //     byeBye();
+
+// // } else {
+// //     other()
+// // }
+
+
+// // const greet = ()=>{
+// //     alert('Hi!');
+// // }
+// // const sayBye = ()=>{
+// //     alert('Bye!');
+// // }
+// // const answer = prompt('Are you arriving or leaving', 'Arriving or Leaving?');
+
+// // if(answer === "Arriving"){
+// //     greet();
+// // } else if(answer === "Leaving") {
+// //     sayBye();
+// // }
+
+// const dontDoIt= () => {
+// alert ('you gonna die') 
+// }
+
+// const itIsOk = () => {
+//     alert ('Oh Well')
+// }
+
+// const notSure = () => {
+//     alert ('May be May be not')
+// }
+
+// const safeSafe = () => {
+//     alert ('it is safe')
+// }
+
+// const other = () => {
+//     alert ('get off the road')
+// }
+
+// const response = prompt ('which way do you plan to go?', 'straight, back, right, west')
+
+// if (response === 'straight') {
+//     itIsOk ();
+// }
+
+// if (response === 'back') {
+//     dontDoIt();
+// }
+
+// if (response === 'right') {
+//     notSure();
+// }
+
+// if (response === 'west') {
+//     safeSafe();
+// }
+
+//  else other()
+
+
+    let purse = 0
+    
+    if (currentTool === 'teeth') {
+        purse +=1 } 
+
+    if (purse === 5) {
+        currentTool === 'scissors'
+    }
+
+    if (purse ===25) {
+        currentTool === 'lawnmower'
+    }
+
+
---
+++ unit_1/Week3/New Folder/answer.js
@@ -0,0 +1,41 @@
+//Answer the following
+
+//1. A light switch that can be either on or off.
+//data ype: boolean
+//data structure: let lightSwitch = true;
+
+//2. A A user's email address.
+//data type: string
+//data structure: let emailAddress = 'johndoe@gmail.com'
+
+//3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+//data type: array
+//data structure: const spaceShip ['hull', 'laster blasters', 'factor beam', 'warp drive]
+
+//4. A list of student names from our class
+//data type: array
+//data structure: const studentNames = ['Kate', 'John', 'Mike', 'Heather', 'Stan', 'Miguel']
+
+//5. A list of student names from our class, each with a location.
+//date type: object
+// date structure: 
+// const studentsInfo = {
+//     Kate: 'Bronx',
+//     John: 'Kingstowne',
+//     Mike: 'Redondo Beach',
+//     Heather: 'D.C',
+//     Stan: 'Woodbridge',
+//     Miguel: 'Franconia'
+// }
+
+//6. 
+//data type: object (array within object)
+//data structure:
+// const studentNamesDetailed = [
+//     {name: 'Kate', location: 'Bronx', favTvShow: '24'},
+//     {name: 'John', location: 'Kingstowne', favTvShow: 'House Of Cards'},
+//     {name: 'Mike', location: 'Redendo Beach', favTvShow: 'Breaking Bad'},
+//     {name: 'Heather', location: 'D.C', favTvShow: 'Blue Lagoon'},
+//     {name: 'Stan', location: 'Woodbridge', favTvShow: 'Monsters'},
+//     {name: 'Kate', location: 'Franconia', favTvShow: 'Black Mirror'},
+// ]
