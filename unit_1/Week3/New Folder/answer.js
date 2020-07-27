ad9831d3752064a1f009dffdd92f8a49b5a358a4
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-07-27 00:21:04 -0400

    objectception complete

--- unit_1/Week3/New Folder/answer.js
+++ unit_1/Week3/New Folder/answer.js
@@ -107,3 +107,25 @@
 //   //6. Add the quote "I'm trying to elevate small talk to medium talk" to Larry's quote array. Confirm that it was added.
 //   crazyObject.larry.quotes.push("I'm trying to elevate small talk to medium talk")
 //   console.log(crazyObject.larry.quotes)
+
+//Object-ception
+const inception = {
+    reality: {
+        dreamLayer1: {
+            dreamLayer2: {
+                dreamLayer3: {
+                    dreamLayer4: {
+                        dreamLayer5: {
+                            dreamLayer6: {
+                                limbo: "Joseph Gordon Levitt"
+                           }
+                        }
+                    }
+                }
+            }
+        }
+    }
+ }
+  //1. Change the value of limbo to null
+  inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo='null'
+  console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo)
