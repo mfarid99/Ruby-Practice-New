11d567005d1eec24aeb26532ac174b88b43c617a
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-07-29 17:05:18 -0400

    Bond Done

--- unit_1/Week3/New Folder/answer.js
+++ unit_1/Week3/New Folder/answer.js
@@ -1,178 +1,178 @@
-//Answer the following
+// //Answer the following
 
-//1. A light switch that can be either on or off.
-//data ype: boolean
-//data structure: let lightSwitch = true;
+// //1. A light switch that can be either on or off.
+// //data ype: boolean
+// //data structure: let lightSwitch = true;
 
-//2. A A user's email address.
-//data type: string
-//data structure: let emailAddress = 'johndoe@gmail.com'
+// //2. A A user's email address.
+// //data type: string
+// //data structure: let emailAddress = 'johndoe@gmail.com'
 
-//3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
-//data type: array
-//data structure: const spaceShip ['hull', 'laster blasters', 'factor beam', 'warp drive]
+// //3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// //data type: array
+// //data structure: const spaceShip ['hull', 'laster blasters', 'factor beam', 'warp drive]
 
-//4. A list of student names from our class
-//data type: array
-//data structure: const studentNames = ['Kate', 'John', 'Mike', 'Heather', 'Stan', 'Miguel']
+// //4. A list of student names from our class
+// //data type: array
+// //data structure: const studentNames = ['Kate', 'John', 'Mike', 'Heather', 'Stan', 'Miguel']
 
-//5. A list of student names from our class, each with a location.
-//date type: object
-// date structure: 
-// const studentsInfo = {
-//     Kate: 'Bronx',
-//     John: 'Kingstowne',
-//     Mike: 'Redondo Beach',
-//     Heather: 'D.C',
-//     Stan: 'Woodbridge',
-//     Miguel: 'Franconia'
-// }
+// //5. A list of student names from our class, each with a location.
+// //date type: object
+// // date structure: 
+// // const studentsInfo = {
+// //     Kate: 'Bronx',
+// //     John: 'Kingstowne',
+// //     Mike: 'Redondo Beach',
+// //     Heather: 'D.C',
+// //     Stan: 'Woodbridge',
+// //     Miguel: 'Franconia'
+// // }
 
-//6. 
-//data type: object (array within object)
-//data structure:
-// const studentNamesDetailed = [
-//     {name: 'Kate', location: 'Bronx', favTvShow: '24'},
-//     {name: 'John', location: 'Kingstowne', favTvShow: 'House Of Cards'},
-//     {name: 'Mike', location: 'Redendo Beach', favTvShow: 'Breaking Bad'},
-//     {name: 'Heather', location: 'D.C', favTvShow: 'Blue Lagoon'},
-//     {name: 'Stan', location: 'Woodbridge', favTvShow: 'Monsters'},
-//     {name: 'Kate', location: 'Franconia', favTvShow: 'Black Mirror'},
-// ]
+// //6. 
+// //data type: object (array within object)
+// //data structure:
+// // const studentNamesDetailed = [
+// //     {name: 'Kate', location: 'Bronx', favTvShow: '24'},
+// //     {name: 'John', location: 'Kingstowne', favTvShow: 'House Of Cards'},
+// //     {name: 'Mike', location: 'Redendo Beach', favTvShow: 'Breaking Bad'},
+// //     {name: 'Heather', location: 'D.C', favTvShow: 'Blue Lagoon'},
+// //     {name: 'Stan', location: 'Woodbridge', favTvShow: 'Monsters'},
+// //     {name: 'Kate', location: 'Franconia', favTvShow: 'Black Mirror'},
+// // ]
 
-/////////////////////////////////////////
+// /////////////////////////////////////////
 
-//Take it Easy
-//1. Make an array that holds all of the colors of the rainbow.
-// const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigot', 'violet'];
-// //2. Write code that will access "blue" from the rainbow array.
-// console.log(rainbowColors[4])
-//3. Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
-// const moeInfo = {
-//     favoriteFood: 'Sushi',
-//     hobby: 'Watching movies',
-//     town: 'Alexandria',
-//     favDataType: 'objects',
-// }
+// //Take it Easy
+// //1. Make an array that holds all of the colors of the rainbow.
+// // const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigot', 'violet'];
+// // //2. Write code that will access "blue" from the rainbow array.
+// // console.log(rainbowColors[4])
+// //3. Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+// // const moeInfo = {
+// //     favoriteFood: 'Sushi',
+// //     hobby: 'Watching movies',
+// //     town: 'Alexandria',
+// //     favDataType: 'objects',
+// // }
 
-// //4. Write code that will access your hobby from the object that you just created.
-// console.log(moeInfo.hobby)
+// // //4. Write code that will access your hobby from the object that you just created.
+// // console.log(moeInfo.hobby)
 
-//Crazy Object!
-// const crazyObject = {
-//     taco: [{meat: 'steak',
-//            cheese: ['panela', 'queso', 'chihuahua']},
-//            {meat: 'chicken',
-//             salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
-//           ],
-//     larry: {
-//       nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
-//       quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
-//       characters: [{
-//         name: "Jeff",
-//         occupation: "manager"
-//       },
-//       {
-//         name: "funkhauser",
-//         occupation: "tv dude"
-//       },
-//       {
-//         name: "susie",
-//         occupation: "jeffs wife",
-//         favourtieHobby: "Swearing at Larry and Jeff"
-//       },
-//       ]
-//     }
-//   }
-//   //Use crazy Object to log the following.
+// //Crazy Object!
+// // const crazyObject = {
+// //     taco: [{meat: 'steak',
+// //            cheese: ['panela', 'queso', 'chihuahua']},
+// //            {meat: 'chicken',
+// //             salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+// //           ],
+// //     larry: {
+// //       nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+// //       quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+// //       characters: [{
+// //         name: "Jeff",
+// //         occupation: "manager"
+// //       },
+// //       {
+// //         name: "funkhauser",
+// //         occupation: "tv dude"
+// //       },
+// //       {
+// //         name: "susie",
+// //         occupation: "jeffs wife",
+// //         favourtieHobby: "Swearing at Larry and Jeff"
+// //       },
+// //       ]
+// //     }
+// //   }
+// //   //Use crazy Object to log the following.
 
-//   //1."omg my mouth is burning"
+// //   //1."omg my mouth is burning"
 
-//   console.log(crazyObject.taco[1].salsa[5])
+// //   console.log(crazyObject.taco[1].salsa[5])
 
-//   //2. "Pretty pretty prettayyyyy good"
+// //   //2. "Pretty pretty prettayyyyy good"
 
-// //   console.log(crazyObject.larry.quotes[0])
+// // //   console.log(crazyObject.larry.quotes[0])
 
-// //   //3. "Swearing at Larry and Jeff"
-// //   console.log(crazyObject.larry.characters[2].favourtieHobby)
+// // //   //3. "Swearing at Larry and Jeff"
+// // //   console.log(crazyObject.larry.characters[2].favourtieHobby)
 
-// //   //4. "Chicken Teriyaki Boyyyyyy"
+// // //   //4. "Chicken Teriyaki Boyyyyyy"
 
-// //   console.log(crazyObject.larry.nicknames[1])
+// // //   console.log(crazyObject.larry.nicknames[1])
 
-// //   //5. The object the contains the name funkhauser
-// //   console.log((crazyObject.larry.characters[1]))
+// // //   //5. The object the contains the name funkhauser
+// // //   console.log((crazyObject.larry.characters[1]))
 
-// //   //6. Add the quote "I'm trying to elevate small talk to medium talk" to Larry's quote array. Confirm that it was added.
-// //   crazyObject.larry.quotes.push("I'm trying to elevate small talk to medium talk")
-// //   console.log(crazyObject.larry.quotes)
+// // //   //6. Add the quote "I'm trying to elevate small talk to medium talk" to Larry's quote array. Confirm that it was added.
+// // //   crazyObject.larry.quotes.push("I'm trying to elevate small talk to medium talk")
+// // //   console.log(crazyObject.larry.quotes)
 
-// //Object-ception
-// const inception = {
-//     reality: {
-//         dreamLayer1: {
-//             dreamLayer2: {
-//                 dreamLayer3: {
-//                     dreamLayer4: {
-//                         dreamLayer5: {
-//                             dreamLayer6: {
-//                                 limbo: "Joseph Gordon Levitt"
-//                            }
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//     }
-//  }
-//   //1. Change the value of limbo to null
-//   inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo='null'
-//   console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo)
+// // //Object-ception
+// // const inception = {
+// //     reality: {
+// //         dreamLayer1: {
+// //             dreamLayer2: {
+// //                 dreamLayer3: {
+// //                     dreamLayer4: {
+// //                         dreamLayer5: {
+// //                             dreamLayer6: {
+// //                                 limbo: "Joseph Gordon Levitt"
+// //                            }
+// //                         }
+// //                     }
+// //                 }
+// //             }
+// //         }
+// //     }
+// //  }
+// //   //1. Change the value of limbo to null
+// //   inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo='null'
+// //   console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo)
 
-////////Bond Films
+// ////////Bond Films
 
-const bondFilms = [
-    { "title" : "Skyfall", "year" : 2012, "actor" : "Daniel Craig", "gross" : "$1,108,561,008" },
-    { "title" : "Thunderball", "year" : 1965, "actor" : "Sean Connery", "gross" : "$1,014,941,117" },
-    { "title" : "Goldfinger", "year" : 1964, "actor" : "Sean Connery", "gross" : "$912,257,512" },
-    { "title" : "Live and Let Die", "year" : 1973, "actor" : "Roger Moore", "gross" : "$825,110,761" },
-    { "title" : "You Only Live Twice", "year" : 1967, "actor" : "Sean Connery", "gross" : "$756,544,419" },
-    { "title" : "The Spy Who Loved Me", "year" : 1977, "actor" : "Roger Moore", "gross" : "$692,713,752" },
-    { "title" : "Casino Royale", "year" : 2006, "actor" : "Daniel Craig", "gross" : "$669,789,482" },
-    { "title" : "Moonraker", "year" : 1979, "actor" : "Roger Moore", "gross" : "$655,872,400" },
-    { "title" : "Diamonds Are Forever", "year" : 1971, "actor" : "Sean Connery", "gross" : "$648,514,469" },
-    { "title" : "Quantum of Solace", "year" : 2008, "actor" : "Daniel Craig", "gross" : "$622,246,378" },
-    { "title" : "From Russia with Love", "year" : 1963, "actor" : "Sean Connery", "gross" : "$576,277,964" },
-    { "title" : "Die Another Day", "year" : 2002, "actor" : "Pierce Brosnan", "gross" : "$543,639,638" },
-    { "title" : "Goldeneye", "year" : 1995, "actor" : "Pierce Brosnan", "gross" : "$529,548,711" },
-    { "title" : "On Her Majesty's Secret Service", "year" : 1969, "actor" : "George Lazenby", "gross" : "$505,899,782" },
-    { "title" : "The World is Not Enough", "year" : 1999, "actor" : "Pierce Brosnan", "gross" : "$491,617,153" },
-    { "title" : "For Your Eyes Only", "year" : 1981, "actor" : "Roger Moore", "gross" : "$486,468,881" },
-    { "title" : "Tomorrow Never Dies", "year" : 1997, "actor" : "Pierce Brosnan", "gross" : "$478,946,402" },
-    { "title" : "The Man with the Golden Gun", "year" : 1974, "actor" : "Roger Moore", "gross" : "$448,249,281" },
-    { "title" : "Dr. No", "year" : 1962, "actor" : "Sean Connery", "gross" : "$440,759,072" },
-    { "title" : "Octopussy", "year" : 1983, "actor" : "Roger Moore", "gross" : "$426,244,352" },
-    { "title" : "The Living Daylights", "year" : 1987, "actor" : "Timothy Dalton", "gross" : "$381,088,866" },
-    { "title" : "A View to a Kill", "year" : 1985, "actor" : "Roger Moore", "gross" : "$321,172,633" },
-    { "title" : "License to Kill", "year" : 1989, "actor" : "Timothy Dalton", "gross" : "$285,157,191" }
-  ];
+// const bondFilms = [
+//     { "title" : "Skyfall", "year" : 2012, "actor" : "Daniel Craig", "gross" : "$1,108,561,008" },
+//     { "title" : "Thunderball", "year" : 1965, "actor" : "Sean Connery", "gross" : "$1,014,941,117" },
+//     { "title" : "Goldfinger", "year" : 1964, "actor" : "Sean Connery", "gross" : "$912,257,512" },
+//     { "title" : "Live and Let Die", "year" : 1973, "actor" : "Roger Moore", "gross" : "$825,110,761" },
+//     { "title" : "You Only Live Twice", "year" : 1967, "actor" : "Sean Connery", "gross" : "$756,544,419" },
+//     { "title" : "The Spy Who Loved Me", "year" : 1977, "actor" : "Roger Moore", "gross" : "$692,713,752" },
+//     { "title" : "Casino Royale", "year" : 2006, "actor" : "Daniel Craig", "gross" : "$669,789,482" },
+//     { "title" : "Moonraker", "year" : 1979, "actor" : "Roger Moore", "gross" : "$655,872,400" },
+//     { "title" : "Diamonds Are Forever", "year" : 1971, "actor" : "Sean Connery", "gross" : "$648,514,469" },
+//     { "title" : "Quantum of Solace", "year" : 2008, "actor" : "Daniel Craig", "gross" : "$622,246,378" },
+//     { "title" : "From Russia with Love", "year" : 1963, "actor" : "Sean Connery", "gross" : "$576,277,964" },
+//     { "title" : "Die Another Day", "year" : 2002, "actor" : "Pierce Brosnan", "gross" : "$543,639,638" },
+//     { "title" : "Goldeneye", "year" : 1995, "actor" : "Pierce Brosnan", "gross" : "$529,548,711" },
+//     { "title" : "On Her Majesty's Secret Service", "year" : 1969, "actor" : "George Lazenby", "gross" : "$505,899,782" },
+//     { "title" : "The World is Not Enough", "year" : 1999, "actor" : "Pierce Brosnan", "gross" : "$491,617,153" },
+//     { "title" : "For Your Eyes Only", "year" : 1981, "actor" : "Roger Moore", "gross" : "$486,468,881" },
+//     { "title" : "Tomorrow Never Dies", "year" : 1997, "actor" : "Pierce Brosnan", "gross" : "$478,946,402" },
+//     { "title" : "The Man with the Golden Gun", "year" : 1974, "actor" : "Roger Moore", "gross" : "$448,249,281" },
+//     { "title" : "Dr. No", "year" : 1962, "actor" : "Sean Connery", "gross" : "$440,759,072" },
+//     { "title" : "Octopussy", "year" : 1983, "actor" : "Roger Moore", "gross" : "$426,244,352" },
+//     { "title" : "The Living Daylights", "year" : 1987, "actor" : "Timothy Dalton", "gross" : "$381,088,866" },
+//     { "title" : "A View to a Kill", "year" : 1985, "actor" : "Roger Moore", "gross" : "$321,172,633" },
+//     { "title" : "License to Kill", "year" : 1989, "actor" : "Timothy Dalton", "gross" : "$285,157,191" }
+//   ];
 
-  //1- Create a new array called bondTitles with only the titles of the Bond films, and console.log the new array.
+//   //1- Create a new array called bondTitles with only the titles of the Bond films, and console.log the new array.
 
-//   bondTitles = [] ;
-//   for (let movies of bondFilms) {
-//       bondTitles.push(movies.title)
-//   }
-//   console.log(bondTitles)
+// //   bondTitles = [] ;
+// //   for (let movies of bondFilms) {
+// //       bondTitles.push(movies.title)
+// //   }
+// //   console.log(bondTitles)
 
-  //2-Create a new array oddBonds, of only the Bond films released on odd-numbered years.
-//   oddBonds = [];
-//   for (let oddYearMovies of bondFilms) {
-//       if (oddYearMovies.year % 2 !==0) {
-//           oddBonds.push(oddYearMovies)
-//       }
-//   }
-//   console.log(oddBonds)
+//   //2-Create a new array oddBonds, of only the Bond films released on odd-numbered years.
+// //   oddBonds = [];
+// //   for (let oddYearMovies of bondFilms) {
+// //       if (oddYearMovies.year % 2 !==0) {
+// //           oddBonds.push(oddYearMovies)
+// //       }
+// //   }
+// //   console.log(oddBonds)
 
-//3-Need Help
+// //3-Need Help
