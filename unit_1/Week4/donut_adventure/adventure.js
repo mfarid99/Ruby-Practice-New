f81331e0ca2b0aeb4b7457e1a910361877892ce0
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-08-02 15:38:17 -0400

    created our enemy pizza rat

--- unit_1/Week4/donut_adventure/adventure.js
+++ unit_1/Week4/donut_adventure/adventure.js
@@ -9,9 +9,8 @@ class Hero {
     }
 }
 catchPhrases(char1) {
-    console.log('i\'m fresher than day old pizza', 
-
-    'you can\'t count my calories')
+    console.log(['i\'m fresher than day old pizza', 
+    'you can\'t count my calories'])
 }
 talkSass(char1) { 
     console.log(catchPhrases().random)
@@ -24,5 +23,33 @@ fight(char1) {
     console.log('i\'m ready to rumble')
 }
 }
+
+class Enemy {
+    constructor (name) {
+    this.name = name
+    this. health = 100
+    this.weapons = {
+            pepperoniStars: 5,
+            cheeseGrease: 10    
+    
+    }
+}
+    catchPhrases(char2) {
+        console.log(['i\'m youtube famous',
+'i\'m more dangerous than an uncovered sewer'])
+    }
+    talkSmack(char2) {
+        console.log(this.catchPhrases().random)
+    }
+    announceHealth(char2) {console.log()}
+    ////announceHealth: logs his current health. Unclear
+    }
+    fight(char2) {
+        console.log('i\'m gonna flatten you like a slice of pepperoni!')
+    }
+
+    class PizzaRat extends Enemy {
+        
+    }
 
 
