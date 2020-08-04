3acc9a677ccd7f5bbeba1d25852edd6c11b4733e
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-08-04 17:53:24 -0400

    Created enemy pizza rat

--- unit_1/Week4/donut_adventure/adventure.js
+++ unit_1/Week4/donut_adventure/adventure.js
@@ -1,67 +1,64 @@
-class Hero {
-    constructor (name, health=100, 
-        weapons = {
-        sprinkleSpray: 5,
-        sugarShock: 10
-        }) {
-    this.name = name;
-    this.health = health;
-    this.weapons = weapons;
-    this.catchPhrases = ['i\'m fresher than day old pizza', 
-    'you can\'t count my calories']
-        }
+    // class Hero {
+    // constructor (name) {
+    // this.name = name;
+    // this.health = 100;
+    // this.weapons = {
+    //     sprinkleSpray: 5,
+    //     sugarShock: 10
+    //     };
+    // this.catchPhrases = ['i\'m fresher than day old pizza', 
+    // 'you can\'t count my calories']
+    //     }
 
-    //Methods
+    // //Methods
 
-    talkSass() {
-        console.log(this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)])
-    //Getting error msg that catchPhrases is not defined
+    // talkSass() {
+    //     console.log(this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)])
 
-    }
+    // }
 
-    announceHealth() {
-    console.log(this.health)
+    // announceHealth() {
+    // console.log(this.health)
 
-    }
-    fight() {
-    console.log('i\'m ready to rumble')
-    }
-    }
+    // }
+    // fight() {
+    // console.log('i\'m ready to rumble')
+    // }
+    // }
 
-    const Dougie = new Hero ('big rat', 200)
+    // const Dougie = new Hero ('big rat', 200)
 
-    console.log(Dougie)
-    console.log(Dougie.talkSass())
-    console.log(Dougie.announceHealth())
-    console.log(Dougie.fight())
-// console.log(rat.talkSass())
-// class Enemy {
-//     constructor (name) {
-//     this.name = name
-//     this. health = 100
-//     this.weapons = {
-//             pepperoniStars: 5,
-//             cheeseGrease: 10    
+    // console.log(Dougie)
+    // console.log(Dougie.talkSass())
+    // console.log(Dougie.announceHealth())
+    // console.log(Dougie.fight())
+
+
+class Enemy {
+    constructor (name) {
+    this.name = name,
+    this.health = 100,
+    this.weapons = {
+            pepperoniStars: 5,
+            cheeseGrease: 10 } ,
+    this.catchPhrases = ['i\'m youtube famous',
+    'i\'m more dangerous than an uncovered sewer']
     
-//     }
-// }
+    }
 
-//     catchPhrases(char2) {
-//         console.log(['i\'m youtube famous',
-// 'i\'m more dangerous than an uncovered sewer'])
-//     }
-//     talkSmack(char2) {
-//         console.log(this.catchPhrases().random)
-//     }
-//     announceHealth(char2) {console.log()}
-//     ////announceHealth: logs his current health. Unclear
-//     }
-//     fight(char2) {
-//         console.log('i\'m gonna flatten you like a slice of pepperoni!')
-//     }
+    talkSmack() {
+        console.log(this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)])
+    }
 
-//     class PizzaRat extends Enemy {
-        
-//     }
+    announceHealth() {console.log(this.health)
+   
+    }
+    
+    fight() {
+        console.log('i\'m gonna flatten you like a slice of pepperoni!')
+    }
+    }
 
-
+    const wahoo = new Enemy ('Fawzi')
+    console.log(wahoo)
+    console.log(wahoo.fight())
