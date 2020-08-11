923fe683c0cc5d381be63c5f7ae6994af7459972
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-08-10 23:56:17 -0400

    All files linked and loaded

---
+++ unit_1/Week5/hogwarts/app.js
@@ -0,0 +1,48 @@
+// // // document.querySelector('h1').innerText = 'Welcome to the Upside Down';
+
+// // // const image = document.querySelector('img')
+// // // console.log(image)
+// // // image.style.display = 'none'
+// // // image.style.display = 'block'
+// // // image.style.filter = 'grayscale(60%)'
+// // // image.style.width = '150%'
+// // // image.style.filter = ''
+// // // image.style.width = ''
+
+// // // const eggos = document.createElement('img')
+// // // document.querySelector('.container').appendChild(eggos)
+// // // eggos.setAttribute('src', 'https://i.ytimg.com/vi/TgSmPqMGk2g/maxresdefault.jpg')
+// // // eggos.style.width = '80%'
+
+// // // eggos.remove()
+
+// // // const liCollection = document.querySelectorAll('li')
+// // // console.log(liCollection)
+// // // document.querySelectorAll('li')[2].innerText = 'Demo Dog Hangout'
+
+// // // document.querySelectorAll('li')[3].remove()
+
+// // // $('.container')
+// // // console.log( $ )
+
+// // const $div = $('<div>');
+// // console.log($div)
+
+// // $('body')
+// // console.log($('body'))
+
+// // $('body').append( $div );
+// // $div.addClass('meat');
+
+// // $('div')
+// // console.log($('div'))
+
+// // const $divs = $('div');
+// // console.log($divs)
+// // // $divs.hide()
+
+// // // -$divs.hide( 'slow' );
+
+// if (typeof jQuery == 'undefined'){
+//     console.log('oops! I still have to link my jQuery properly!');
+//   } else {console.log('I did it! I linked jQuery and this js file!')};
