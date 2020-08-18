ff38572280571771417fb95758dc65faec5246f9
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-08-18 16:31:12 -0400

    Chapter 11 complete - Sam and Frodo are in Mordor and Mount Doom has been created

--- unit_1/Week6/lotr/fellowship.js
+++ unit_1/Week6/lotr/fellowship.js
@@ -338,13 +338,22 @@ const hornOfGondor = (() => {
 // ============
 // Chapter 11
 // ============
-const itsDangerousToGoAlone = () => {
+
+const $frodobag = $('#the-fellowship').children().not('h1').eq(0)
+const $samW = $('#the-fellowship').children().not('h1').eq(1)
+
+
+const itsDangerousToGoAlone = (() => {
+  
+  $('#Mordor').append($frodobag)
+  $('#Mordor').append($samW)
 
   // 1. take Frodo and Sam out of the fellowship and move them to Mordor (they don't need to be inside a ul in Mordor)
 
   // 2. add a div with an id of 'mount-doom' to Mordor
 
-};
+});
+itsDangerousToGoAlone()
 
 // COMMIT YOUR WORK
 // The commit message should read: "Chapter 11 complete - Sam and Frodo are in Mordor and Mount Doom has been created"
