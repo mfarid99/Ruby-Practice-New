5ca59fa5e674b5084cba04aba499124e197c0866
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-09-09 12:53:35 -0400

    Magic 8 Ball Express Application Created

--- unit_2/W0902/server.js
+++ unit_2/W0902/server.js
@@ -9,12 +9,29 @@ const port = 3000;
 // 	res.send(`hello ${req.params.name} it's great to see you finally`);
 // });
 
-app.get('/tip/:total/:tipPercentage', (req, res) => {
+// app.get('/tip/:total/:tipPercentage', (req, res) => {
     
-res.send(`${req.params.tipPercentage/req.params.total * 100}`);
-});
+// res.send(`${req.params.tipPercentage/req.params.total * 100}`);
+// });
 
 
+
+  
+
+const questions = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again l0ater","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+
+
+app.get('/:magic', (req, res) => {
+let question = questions[req.params.magic] 
+// const randomsness = (questions) => {
+// return questions [Math.floor(Math.random()*questions.length)]
+// }
+
+    // let question = "Will I be a Millionaire";
+    res.send(questions[Math.floor(Math.random()*questions.length)]);
+
+
+})
 
 
 
