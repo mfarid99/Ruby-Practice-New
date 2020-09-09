d253897449f191e706d83f602849682ee085a903
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-09-09 12:59:57 -0400

    Magic 8 Ball express application created

--- unit_2/W0902/server.js
+++ unit_2/W0902/server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
+//Greetings
 
 // app.get('/greeting/:name', (req, res) => {
-// 	console.log(req.params);
 // 	res.send(`hello ${req.params.name} it's great to see you finally`);
 // });
+
+//Calculator
 
 // app.get('/tip/:total/:tipPercentage', (req, res) => {
     
@@ -15,7 +16,7 @@ const port = 3000;
 // });
 
 
-
+// Magic 8 Ball
   
 
 const questions = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again l0ater","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
@@ -23,12 +24,7 @@ const questions = ["It is certain", "It is decidedly so", "Without a doubt", "Ye
 
 app.get('/:magic', (req, res) => {
 let question = questions[req.params.magic] 
-// const randomsness = (questions) => {
-// return questions [Math.floor(Math.random()*questions.length)]
-// }
-
-    // let question = "Will I be a Millionaire";
-    res.send(questions[Math.floor(Math.random()*questions.length)]);
+res.send("Will I be a millionaire? " +  questions[Math.floor(Math.random()*questions.length)]);
 
 
 })
