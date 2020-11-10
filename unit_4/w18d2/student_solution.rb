8e38643e973cc9b7ee18331e72da6f1ccb42d324
moe <moe.faridd@gmail.com>
2020-11-09 20:23:52 -0500

    Getting Started

---
+++ unit_4/w18d2/student_solution.rb
@@ -0,0 +1,14 @@
+# 1- Print "Hello World" to the console
+# p "Hello World"
+#==> "Hello World
+
+=begin
+adjective = "Big Bad"
+2-Interpolate the adjective variable into the Hello World string using #{}
+p "Hello #{adjective} World"
+=end
+#==> "Hello Big Bad World"
+
+x = "Hello World"
+p x.upcase
+
